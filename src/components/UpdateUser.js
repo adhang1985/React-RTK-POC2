3ad712be17fb97d8f759c5775d85e1c838f7b91b
dhangabh <abhishek.dhang.ext@boehringer-ingelheim.com>
2024-03-23 18:52:51 +0530

    corrected code

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-import { editUser, getOneUser } from '../middleware/userServices';
+import { editUser } from '../middleware/userServices';
 
 const UpdateUser = () => {
 
